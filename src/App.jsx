9024fc64c197fb3721ddb5889d1f1c6c571d8136
Exileurt <favrel.aurelien@gmail.com>
2023-11-09 23:32:11 +0100

    init-repository

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import RouterPages from './pages/RouterPages';
 import ScrollDriven from './components/scrollDriven/scrollDriven';
+import gsap from 'gsap';
+import {useLayoutEffect, useRef} from 'react';
 
 const App = () => {
+  const cursor = useRef();
+  useLayoutEffect(() => {
+    gsap.set('.flair', {xPercent: -50, yPercent: -50});
+    let xTo = gsap.quickTo(cursor.current, 'x', {
+      duration: 2.5,
+      ease: 'back.out(1.7)',
+    });
+    let yTo = gsap.quickTo(cursor.current, 'y', {
+      duration: 2.5,
+      ease: 'back.out(1.7)',
+    });
+    window.addEventListener('mousemove', e => {
+      xTo(e.clientX);
+      yTo(e.clientY);
+    });
+  }, []);
+
   return (
-    <div className="h-screen relative bg-gradient-to-r from-very-dark-yellow to-black">
+    <div className="h-screen bg-gradient-to-r from-very-dark-yellow to-black">
       <Router>
+        <div
+          ref={cursor}
+          className="relative -top-2 -left-2 w-4 h-4 rounded-full bg-amber-400 z-50"
+        ></div>
         <ScrollDriven />
         <Routes>
           <Route path="/*" element={<RouterPages />} />
