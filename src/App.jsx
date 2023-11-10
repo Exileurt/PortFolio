540483c1c5e6e622630f6f46b39f4f6a0c84a613
Exileurt <favrel.aurelien@gmail.com>
2023-11-10 20:06:19 +0100

    fix mouse on link

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import RouterPages from './pages/RouterPages';
 import ScrollDriven from './components/scrollDriven/scrollDriven';
+import Cursor from './components/Cursor/Cursor';
 
 const App = () => {
   return (
     <div className="h-screen bg-gradient-to-r from-very-dark-yellow to-black">
       <Router>
+        <Cursor />
         <ScrollDriven />
         <Routes>
           <Route path="/*" element={<RouterPages />} />
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,23 +5,7 @@ import {TextPlugin} from 'gsap/TextPlugin';
 gsap.registerPlugin(TextPlugin);
 
 const Home = () => {
-  //CURSOR
-  const cursor = useRef();
-  useLayoutEffect(() => {
-    gsap.set(cursor.current, {xPercent: -50, yPercent: -50});
-    let xTo = gsap.quickTo(cursor.current, 'x', {
-      duration: 1.5,
-      ease: 'back.out(1.7)',
-    });
-    let yTo = gsap.quickTo(cursor.current, 'y', {
-      duration: 1.5,
-      ease: 'back.out(1.7)',
-    });
-    window.addEventListener('mousemove', e => {
-      xTo(e.clientX);
-      yTo(e.clientY);
-    });
-  }, []);
+  const cursorHover = useRef();
 
   //TEXT ANIMATION
   const boxRef = useRef();
@@ -45,15 +29,14 @@ const Home = () => {
 
   return (
     <div>
-      <span
-        ref={cursor}
-        className="absolute w-6 h-6 border border-amber-400 rounded-full z-50"
-      />
       <Menu />
 
       <div className="flex flex-col">
         <div className="h-screen flex flex-col justify-center pl-36 select-none">
-          <h1 className="font-semibold text-9xl tracking-[19px] bg-gradient-to-r from-amber-500  to-amber-400 text-transparent bg-clip-text">
+          <h1
+            id={cursorHover}
+            className="font-semibold text-9xl tracking-[19px] bg-gradient-to-r from-amber-500  to-amber-400 text-transparent bg-clip-text"
+          >
             AURELIEN
             <br />
             FAVREL
