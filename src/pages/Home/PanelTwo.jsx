ba862aedabc9236ee2ffcbcf5498b2ee15836804
Exileurt <favrel.aurelien@gmail.com>
2023-11-19 19:37:46 +0100

    add bg

--- src/pages/Home/PanelTwo.jsx
+++ src/pages/Home/PanelTwo.jsx
@@ -1,14 +1,14 @@
 import gsap from 'gsap';
 import {ScrollTrigger, TextPlugin} from 'gsap/all';
 import {useLayoutEffect, useContext} from 'react';
-import {CursorContext} from '../../components/Cursor/cursor';
-import armenian from '../../assets/armenian.png';
+// import {CursorContext} from '../../components/Cursor/cursor';
+
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(TextPlugin);
 
 const PanelTwo = () => {
   //CURSOR
-  const {onEnterCursor, onLeaveCursor} = useContext(CursorContext);
+  //const {onEnterCursor, onLeaveCursor} = useContext(CursorContext);
   //ANIMATION
   useLayoutEffect(() => {
     const tl = gsap.timeline();
@@ -29,19 +29,10 @@ const PanelTwo = () => {
   });
 
   return (
-    <div className="panelTwo hidden fixed justify-center items-center flex-col text-center top-0 left-0 h-screen w-full bg-gradient-to-r">
-      <h2 className="text-6xl font-bold text-amber-400 pb-12">
-        DIFFERENT STYLE OF WEBSITE
+    <div className="panelTwo bg-amoccidental bg-cover hidden fixed justify-center flex-col text-center top-0 left-0 h-screen w-full  backdrop-blur-sm border-4 border-amber-400">
+      <h2 className="font-bold text-6xl text-amber-400 max-w-3xl text-left ml-32">
+        I USE THESE SKILL TO CREATE VARIOUS WEBSITES, LIKE LEARNING WEBSITE..
       </h2>
-      <div className="fixed left-96 flex justify-center">
-        <img
-          src={armenian}
-          alt="armenian"
-          className="imgOne hidden w-1/2 h-auto border-2 border-amber-400"
-          onMouseEnter={onEnterCursor}
-          onMouseLeave={onLeaveCursor}
-        />
-      </div>
     </div>
   );
 };
